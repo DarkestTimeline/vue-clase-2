b481647d468ce40a4766b7e1edfa02ff45738905
DarkestTimeline <sfvasquez96@gmail.com>
2022-08-15 19:54:53 -0600

    Nav bar

--- src/App.vue
+++ src/App.vue
@@ -7,27 +7,35 @@
   <!-- <ejemploFiguras/>-->
 <!--</div> -->
 <h1 id="title">Ejercicio #3 - Operaciones Básicas de Matrices en VUE</h1>
+<ul>
+  <li><a href="#syr">Suma y Resta</a></li>
+  <li><a href="#ide">Matriz Identidad y Matriz Nula</a></li>
+  <li><a href="#pot">Potencia de Matriz</a></li>
+  <li><a href="#tra">Matriz Transpuesta</a></li>
+  <li><a href="#inv">Inversa</a></li>
+</ul>
+
 <div id="matrices">
-  <h1>Inciso #1</h1>
+  <h1 id="syr">Inciso #1</h1>
   <MatrizSumaResta/>
-  <h1>Inciso #2</h1>
+  <h1 id="ide">Inciso #2</h1>
   <matrizIdentidad/>
   <matrizNula/>
-  <h1>Inciso #3</h1>
+  <h1 id="pot">Inciso #3</h1>
   <matrizPot/>
   <matrizPE/>
-  <h1>Inciso #4</h1>
+  <h1 id="tra">Inciso #4</h1>
   <matrizTranspuesta/>
-  <h1>Inciso #5</h1>
-<matrizGJ/>
+  <h1 id="inv">Inciso #5</h1>
+  <matrizGJ/>
 </div>
 <h1>Grupo #5 - Sistemas Expertos</h1>
-<ul>
+<ol>
   <li><strong>Sergio Fabricio Vasquez Castellanos</strong>		- 20142002711</li>
   <li><strong>Angel Santiago Trochez Briones</strong> 	      - 20151021032</li>
   <li><strong>Alexi Daniel Saravia</strong>				          - 20132007687</li>
   <li><strong>Juan De Dios Carranza Motiño</strong> 		      - 20172030613</li>
-</ul>
+</ol>
 <img alt="" src="./assets/sis_logo.png" id="sis_logo">
 </template>
 
@@ -82,9 +90,30 @@ input[type=radio] {
 span{
     word-spacing: 10px;
   }
-  ul{
-    list-style: none;
-  }
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  color: white;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
